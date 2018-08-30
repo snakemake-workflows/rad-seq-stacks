a75cf2a65aeb3ff7c34fa9062bb4ee2de4e37a79
Henning Timm <henning.timm@tu-dortmund.de>
2018-08-30 12:51:22 +0200

    Added missing space that was obfuscating the correct input file type.

--- scripts/extract-individuals.py
+++ scripts/extract-individuals.py
@@ -4,5 +4,5 @@ for barcode_file, fq1, fq2 in zip(snakemake.input.barcodes, snakemake.input.fq1,
     shell("process_radtags -1 {fq1} -2 {fq2} "
           "--renz_1 {snakemake.params.enzymes[p5]} "
           "--renz_2 {snakemake.params.enzymes[p7]} "
-          "-b {barcode_file} -o {snakemake.params.outdir} -y gzfastq -i gzfastq"
+          "-b {barcode_file} -o {snakemake.params.outdir} -y gzfastq -i gzfastq "
           "{snakemake.params.extra}")
