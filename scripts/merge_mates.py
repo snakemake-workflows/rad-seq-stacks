3c7cf631ce5a530671a6e6232825a092e6837181
Henning Timm <henning.timm@tu-dortmund.de>
2019-04-02 14:01:29 +0200

    Refactored merge rule using the script directive.

--- scripts/merge_mates.py
+++ scripts/merge_mates.py
@@ -1,42 +1,40 @@
+"""Merge the p5 and p7 reads from the input files into one read,
+joined by join bases.
+"""
 import sys
 import click
 import dinopy
 
+# redirect stderr to logfile
+sys.stderr = open(snakemake.log[0], "w")
 
-@click.command(short_help="later")
-@click.argument("p5_file", nargs=1, type=click.Path(exists=True))
-@click.argument("p7_file", nargs=1, type=click.Path(exists=True))
-@click.argument("merged_file", nargs=1, type=click.Path(dir_okay=True))
-@click.option("--padding-bases", "padding_bases", default=b"NNNNN")
-@click.option("--padding-quality", "padding_quality", default=b"!")
-def merge_mates(p5_file, p7_file, merged_file, padding_bases, padding_quality):
-    """Merge the p5 and p7 reads from the input files into one read,
-    joined by padding bases.
-    """
-    print(f"Opening files:\n    {p5_file}\n    {p7_file}")
-    print(f"Writing to:\n    {merged_file}")
-    p5_reader = dinopy.FastqReader(p5_file)
-    p7_reader = dinopy.FastqReader(p7_file)
+# get and open input files from the calling snakemake rules input directive
+p5_file, p7_file = snakemake.input
+print(f"Opening files:\n    {p5_file}\n    {p7_file}", file=sys.stderr)
+print(f"Writing to:\n    {snakemake.output.merged}", file=sys.stderr)
+p5_reader = dinopy.FastqReader(p5_file)
+p7_reader = dinopy.FastqReader(p7_file)
 
-    if len(padding_quality) != 1:
-        print("Please specify a single Sanger Phred+33 quality value for "
-              "padding.\nExample: --padding-quality !")
-        sys.exit(1)
-    else:
-        padding_quality = padding_quality.encode()
+# check if the quality value for the join sequence is valid
+join_quality = snakemake.params.join_quality
+if len(join_quality) != 1:
+    print("Please specify a single Sanger Phred+33 quality value for "
+          "join_quality.", file=sys.stderr)
+    sys.exit(1)
+else:
+    join_quality = join_quality.encode()
 
-    padding_bases = padding_bases.encode()
-    padding_qvs = padding_quality * len(padding_bases)
-    with dinopy.FastqWriter(merged_file, force_overwrite=True) as writer:
-        for p5_read, p7_read in zip(p5_reader.reads(), p7_reader.reads()):
-            merged_seq = b"".join(
-                (p5_read.sequence, padding_bases, p7_read.sequence)
-            )
-            merged_qvs = b"".join(
-                (p5_read.quality, padding_qvs, p7_read.quality)
-            )
-            writer.write(merged_seq, p5_read.name, merged_qvs)
+# Assemble join sequence and quality values
+join_seq = snakemake.params.join_seq.encode()
+join_qvs = (snakemake.params.join_quality * len(join_seq)).encode()
 
-
-if __name__ == "__main__":
-    merge_mates()
+# join all reads
+with dinopy.FastqWriter(snakemake.output.merged, force_overwrite=True) as writer:
+    for p5_read, p7_read in zip(p5_reader.reads(), p7_reader.reads()):
+        merged_seq = b"".join(
+            (p5_read.sequence, join_seq, p7_read.sequence)
+        )
+        merged_qvs = b"".join(
+            (p5_read.quality, join_qvs, p7_read.quality)
+        )
+        writer.write(merged_seq, p5_read.name, merged_qvs)
